cce5a0c718b254559434f789dd96b3c3aad78e5a
anujks1004 <anuj.singh100420@gmail.com>
2023-12-05 23:31:01 +0530

    navbar working

--- Navbar/app.js
+++ Navbar/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const btn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+btn.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
