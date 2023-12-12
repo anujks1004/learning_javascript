11a602aa542e09b10cf7596c5e454566cb247e35
anujks1004 <anuj.singh100420@gmail.com>
2023-12-13 00:39:13 +0530

    future date made dynamic

--- Countdown/app.js
+++ Countdown/app.js
@@ -21,3 +21,13 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+
+const futureDate = new Date(2023, 11, 25, 11, 30, 0);
+
+giveaway.textContent = `Giveaway Ends On ${
+  weekdays[futureDate.getDay()]
+}, ${futureDate.getDate()} ${
+  months[futureDate.getMonth()]
+} ${futureDate.getFullYear()} ${futureDate.getHours()}:${futureDate.getMinutes()}am`;
