80dfc669ddc658368d5c71732d6ba9cf561c7304
anujks1004 <anuj.singh100420@gmail.com>
2023-12-14 01:29:36 +0530

    able to add item successfully

--- Grocery-bud/app.js
+++ Grocery-bud/app.js
@@ -24,7 +24,26 @@ function addItem(e) {
   const value = grocery.value;
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-  } else if (value) {
+    const element = document.createElement("article");
+    element.classList.add("grocery-item");
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    list.appendChild(element);
+    displayAlert("item added to the list", "success");
+    container.classList.add("show-container");
+    addToLocalStorage(id, value);
+    setBackToDefault();
+  } else if (value && editFlag) {
   } else {
     displayAlert("Please enter value", "danger");
   }
