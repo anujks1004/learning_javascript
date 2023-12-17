ddfa60bf9ebc5ddfc4090daa599e5a29922dfb52
anujks1004 <anuj.singh100420@gmail.com>
2023-12-17 23:28:17 +0530

    edit local storage added

--- Grocery-bud/app.js
+++ Grocery-bud/app.js
@@ -117,4 +117,16 @@ function getLocalStorage() {
     : [];
 }
 
+function editLocalStorage(id, value) {
+  let items = getLocalStorage();
+
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
 // ****** SETUP ITEMS **********
