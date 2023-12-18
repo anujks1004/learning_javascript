88c4ad385204c140a87a8b07730d6d36d568f602
anujks1004 <anuj.singh100420@gmail.com>
2023-12-18 23:38:11 +0530

    all the functionalities of the project implemented

---
+++ Slider/app.js
@@ -0,0 +1,26 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  if (counter == 0) counter = slides.length - 1;
+  else if (counter == slides.length) counter = 0;
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
