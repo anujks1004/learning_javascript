ac3af06cdfe64e2df825e61931ac07debadeaa8c
anujks1004 <anuj.singh100420@gmail.com>
2023-12-25 17:27:38 +0530

    moment.js library added and date formatted using the library

--- Dark-mode/app.js
+++ Dark-mode/app.js
@@ -7,10 +7,11 @@ toggleBtn.addEventListener("click", () => {
 
 const articlesData = articles
   .map((item) => {
+    const formatDate = moment(item.date).format("MMMM Do YYYY");
     return `<article class="post">
       <h2>${item.title}</h2>
       <div class="post-info">
-        <span>${item.date}</span>
+        <span>${formatDate}</span>
         <span>${item.length} min read</span>
       </div>
       <p>
