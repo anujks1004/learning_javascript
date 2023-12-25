e7a0134d08649313964b6b9245bde38de5d3a57a
anujks1004 <anuj.singh100420@gmail.com>
2023-12-25 17:15:47 +0530

    data imported from data,js file

--- Dark-mode/app.js
+++ Dark-mode/app.js
@@ -1,5 +1,23 @@
 const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
 
 toggleBtn.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-theme");
 });
+
+const articlesData = articles
+  .map((item) => {
+    return `<article class="post">
+      <h2>${item.title}</h2>
+      <div class="post-info">
+        <span>${item.date}</span>
+        <span>${item.length} min read</span>
+      </div>
+      <p>
+        ${item.snippet}
+      </p>
+    </article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
