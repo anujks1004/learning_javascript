0594697ceb853783685b1bb0e7f4ff0013706f6c
anujks1004 <anuj.singh100420@gmail.com>
2023-12-25 15:24:13 +0530

    number project implemented

--- Numbers/app.js
+++ Numbers/app.js
@@ -1,1 +1,20 @@
-console.log('numbers project');
+const items = document.querySelectorAll(".number");
+
+const updateValue = (element) => {
+  const value = parseInt(element.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue > value) {
+      element.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    element.textContent = `${initialValue}+`;
+  }, 1);
+};
+
+items.forEach((item) => {
+  updateValue(item);
+});
