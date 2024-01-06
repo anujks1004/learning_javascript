974618a7d8f6922fc6058b06152ecae8c42f44b2
anujks1004 <anuj.singh100420@gmail.com>
2024-01-07 00:49:37 +0530

    items fetched and displayed

--- Products/app.js
+++ Products/app.js
@@ -1,1 +1,38 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productsDOM.innerHTML = `<p class="error">there was an error</p>    `;
+  }
+};
+
+const displayProducts = (list) => {
+  const productList = list
+    .map((item) => {
+      return `<a href="product.html" class="single-product">
+            <img src="${
+              item.fields.image[0].url
+            }" alt="" class="single-product-img img" />
+            <footer>
+              <h5 class="name">${item.fields.name}</h5>
+              <span class="price">$${item.fields.price / 100}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = `<div class="products-container">${productList}</div>`;
+};
+
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
