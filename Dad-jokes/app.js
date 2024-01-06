0021ee61729f3557e9a4606014e58eba8ea616c7
anujks1004 <anuj.singh100420@gmail.com>
2024-01-06 20:46:58 +0530

    fetch joke implemented

--- Dad-jokes/app.js
+++ Dad-jokes/app.js
@@ -1,1 +1,20 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/";
+const btn = document.querySelector(".btn");
+const resultJoke = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  fetchJoke();
+});
+
+const fetchJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "learn",
+    },
+  });
+  const data = await response.json();
+  resultJoke.textContent = data.joke;
+};
+
+fetchJoke();
