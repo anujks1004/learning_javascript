12b5ce09c76c522f407e1fa28d3c0b4f3cb3e1d7
anujks1004 <anuj.singh100420@gmail.com>
2024-01-08 01:19:37 +0530

    Project implemented

--- Random-user/app.js
+++ Random-user/app.js
@@ -32,9 +32,27 @@ const getUser = async () => {
   };
 };
 
+const displayUser = (person) => {
+  img.src = person.image;
+  value.textContent = person.name;
+  title.textContent = `My name is`;
+  btns.forEach((btn) => btn.classList.remove("active"));
+  btns[0].classList.add("active");
+
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener("click", () => {
+      value.textContent = person[label];
+      title.textContent = `My ${label} is`;
+      btns.forEach((btn) => btn.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+};
+
 const showUser = async () => {
   const person = await getUser();
-  console.log(person);
+  displayUser(person);
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
