7ad27132478efa6d32e5ead4ff4934dd288cea2f
anujks1004 <anuj.singh100420@gmail.com>
2024-01-17 01:08:41 +0530

    Display followers functionality implemented

--- Pagination/displayFollowers.js
+++ Pagination/displayFollowers.js
@@ -1,3 +1,16 @@
-const display = () => {}
+const container = document.querySelector(".container");
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      return `<article class="card">
+  <img src="${person.avatar_url}" alt="" />
+  <h4>${person.login}</h4>
+  <a href="${person.html_url}" class="btn">view profile</a>
+</article>`;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
+};
+
+export default display;
