2334b4d74e3b9fae1a76a55c67429f2e46a67438
anujks1004 <anuj.singh100420@gmail.com>
2024-01-17 01:09:35 +0530

    Able to paginate the followers

--- Pagination/app.js
+++ Pagination/app.js
@@ -1,4 +1,14 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const title = document.querySelector(".section-title h1");
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  displayFollowers(paginate(followers)[0]);
+  title.textContent = "Pagination";
+};
+
+window.addEventListener("DOMContentLoaded", init);
