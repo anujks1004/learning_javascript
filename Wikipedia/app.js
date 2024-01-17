0bb85b3fc8d26c9e4c03fc9b047dbb61fcbef5cb
anujks1004 <anuj.singh100420@gmail.com>
2024-01-18 00:15:02 +0530

    Fetch data implemented

--- Wikipedia/app.js
+++ Wikipedia/app.js
@@ -3,20 +3,32 @@ const url =
 
 const formDOM = document.querySelector(".form");
 const formInput = document.querySelector(".form-input");
-const results = document.querySelector(".results");
+const resultDOM = document.querySelector(".results");
 
 formDOM.addEventListener("submit", (e) => {
   e.preventDefault();
   const value = formInput.value;
   if (!value) {
-    results.innerHTML = `<div class = "error">Please enter valid search term</div>`;
+    resultDOM.innerHTML = `<div class = "error">Please enter valid search term</div>`;
     return;
   }
   fetchPages(value);
 });
 
 const fetchPages = async (searchValue) => {
-  const response = await fetch(`url${searchValue}`);
-  const data = await response.json();
-  return data;
+  resultDOM.innerHTML = `<div class = "loading"></div>`;
+  try {
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultDOM.innerHTML = `<div class = "error">Please enter valid search term</div>`;
+      return;
+    }
+    renderResults(results);
+  } catch (error) {
+    resultDOM.innerHTML = `<div class = "error">There was an error</div>`;
+  }
 };
+
+const renderResults = (list) => {};
