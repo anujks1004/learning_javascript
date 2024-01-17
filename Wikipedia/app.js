3adb58fb138d8b2598aff1167285bc9ec0da05f7
anujks1004 <anuj.singh100420@gmail.com>
2024-01-18 00:07:35 +0530

    added submit listener to form

--- Wikipedia/app.js
+++ Wikipedia/app.js
@@ -1,1 +1,22 @@
-console.log('wiki starter');
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=";
+
+const formDOM = document.querySelector(".form");
+const formInput = document.querySelector(".form-input");
+const results = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = formInput.value;
+  if (!value) {
+    results.innerHTML = `<div class = "error">Please enter valid search term</div>`;
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  const response = await fetch(`url${searchValue}`);
+  const data = await response.json();
+  return data;
+};
