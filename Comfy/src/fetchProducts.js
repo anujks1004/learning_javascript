218b3bc4652a4f51442744f531137935ccc452e8
anujks1004 <anuj.singh100420@gmail.com>
2024-01-23 00:09:21 +0530

    fetch products function implemented

--- Comfy/src/fetchProducts.js
+++ Comfy/src/fetchProducts.js
@@ -1,5 +1,8 @@
-import { allProductsUrl } from './utils.js';
+import { allProductsUrl } from "./utils.js";
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  const response = await fetch(allProductsUrl);
+  return response.json();
+};
 
 export default fetchProducts;
