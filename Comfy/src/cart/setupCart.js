c69262444de1dfd6cdecc447e6cf7094c6df2614
anujks1004 <anuj.singh100420@gmail.com>
2024-02-09 01:37:22 +0530

    Basic functionality of add to cart working

--- Comfy/src/cart/setupCart.js
+++ Comfy/src/cart/setupCart.js
@@ -5,6 +5,24 @@ import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
 
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id == id);
+  if (!item) {
+    let product = findProduct(id);
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    addToCartDOM(product);
+  } else {
+  }
   openCart();
 };
+
+const init = () => {};
+
+init();
